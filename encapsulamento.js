f92af2d9e3c78349cf0c7972d68cd8ec2a3d20cf
MatheusD <55540506+MatheusDama@users.noreply.github.com>
2022-02-01 10:18:43 -0300

    Encapsulamento

---
+++ encapsulamento.js
@@ -0,0 +1,19 @@
+class Poligono {
+  constructor(altura, largura) {
+    this.altura = altura;
+    this.largura = largura;
+  }
+
+  get area() {
+    return this.#calcularArea();
+  }
+
+  #calcularArea() {
+    return this.altura * this.altura;
+  }
+}
+
+let poligono = new Poligono(50, 60);
+
+console.log(poligono);
+console.log(poligono.area);
